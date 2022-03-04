3e9a8d827108ed595cbcd7508a48476ddaf07c10
firez2469 <hampel.m@northeastern.edu>
2022-03-03 23:52:22 -0500

    Modified the scene a bit

--- Assets/Prefabs/Characters/Player/Scripts/TestMove.cs
+++ Assets/Prefabs/Characters/Player/Scripts/TestMove.cs
@@ -19,48 +19,62 @@ public class TestMove : MonoBehaviour
     private Transform lookAtPointer;
     private Vector3 pPos;
 
+    private bool isJumping = false;
+
     // Start is called before the first frame update
     void Start()
     {
         pPos = transform.position;
     }
 
+    private float x = 0;
+    private float y = 0;
+
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        if (!isJumping)
+        {
+            x = Input.GetAxis("Horizontal");
+            y = Input.GetAxis("Vertical");
+        }
+       
         Vector3 movementVector = controller.transform.forward * y * Time.deltaTime*speed;
         movementVector += controller.transform.right * x * Time.deltaTime*speed;
         if (isRunning)
         {
             movementVector *= runningMultiplier;
         }
-        
 
+       
         controller.Move(movementVector);
+        
+        
         controller.SimpleMove(-controller.transform.up * -9.8f*Time.deltaTime);
         
         lookAtPointer.transform.localPosition = Vector3.Lerp(new Vector3(x, 0, y), lookAtPointer.transform.localPosition,0.5f);
         //anim.transform.LookAt(lookAtPointer);
-        var rotation = Quaternion.LookRotation(lookAtPointer.position - anim.transform.position);
-        anim.transform.rotation = Quaternion.Slerp(anim.transform.rotation, rotation, Time.deltaTime * 8f);
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Mathf.Abs(x) > 0 || Mathf.Abs(y) > 0)
         {
-            isRunning = true;
+            var rotation = Quaternion.LookRotation(lookAtPointer.position - anim.transform.position);
+            anim.transform.rotation = Quaternion.Slerp(anim.transform.rotation, rotation, Time.deltaTime * 8f);
+
         }
 
-
+        
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+       
 
         HandleAnim(x, y);
         pPos = controller.transform.position;
 
     }
 
+
+
     private void HandleAnim(float x,float y)
     {
-        if (Vector3.Distance(controller.transform.position,pPos)>Time.deltaTime*speed)
+        if (Vector3.Distance(controller.transform.position,pPos)>Time.deltaTime*speed*0.8f)
         {
             print("Running!");
             if (Input.GetKey(KeyCode.LeftShift))
