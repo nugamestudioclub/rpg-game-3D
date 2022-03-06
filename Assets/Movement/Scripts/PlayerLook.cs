b91121bc7a470af88e4aa5d795e61a899e8a5694
jritchie02 <jaredr_22@yahoo.com>
2022-03-05 21:08:44 -0500

    First version of FPS movement

---
+++ Assets/Movement/Scripts/PlayerLook.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    Camera cam;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        // only want player to rotate on y axis
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    void HandleInput()
+    {
+        // get the current mouse position
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        // when we rotate horizontally, we move on y axis
+        yRotation += mouseX * sensX * multiplier;
+        // moving on x makes camera look up and down
+        xRotation -= mouseY * sensY * multiplier;
+
+        // clamp so you cant look to far up or down
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
+    }
+}
---
+++ Assets/Movement/Scripts/PlayerMovement.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float moveSpeed = 60f;
+    [SerializeField] float airMultiplier = 0.4f; 
+
+    [Header("Drag")]
+    float groundDrag = 6f;
+    [SerializeField] float airDrag = 2f;
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space; 
+
+    float horizontalMovement;
+    float verticalMovement;
+
+    [Header("Ground Detection")]
+    public float jumpForce = 5f;
+    float playerHeight = 2f; 
+    bool isGrounded;
+    [SerializeField] LayerMask groundMask;
+    float groundDistance = 0.4f;
+    RaycastHit slopeHit; 
+
+    Vector3 moveDirection;
+    Vector3 slopeMoveDirection; 
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, 1, 0), groundDistance, groundMask);
+
+        print(isGrounded);
+        HandleInput();
+        ControlDrag();
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            // Jump
+            Jump();
+        }
+
+        // calculate where to move when on slope based on normals angle
+        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+        {
+            // If the vector is not up, we are on a slope
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+
+    void ControlDrag()
+    {
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
+    }
+
+    void HandleInput()
+    {
+        // Get the input, 1 or -1 for left/right up/down
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        // use transform. to move the player relative to where they are looking
+        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+    }
+
+    
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (isGrounded && !OnSlope())
+        {
+            // normalize move direction so it never goes past 1 on diagonal
+            rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Acceleration);
+        }
+        else if (isGrounded && OnSlope())
+        {
+            rb.AddForce(slopeMoveDirection.normalized * moveSpeed, ForceMode.Acceleration);
+        }
+        else  if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Acceleration);
+        }
+    }
+}
